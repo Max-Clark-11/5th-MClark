1762eeceb29685a598a59200eea529c6437075e0
Max Clark <mc130298@student.wpsok.org>
2026-08-26 13:14:49 -0500

    Summited HW3

---
+++ HW3.py
@@ -0,0 +1,47 @@
+#Max Clark
+#5th Hour
+#HW3
+
+
+#1. Print "Hello World!"
+
+print("Hello World")
+
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+
+Lucky_Number = 7
+Message = "Have a good day!"
+Answer = True
+
+#3. Print all three variables on the same print function (at the same time).
+
+print(Lucky_Number , Message , Answer)
+
+#4. Create a variable that asks the user to input an integer.
+
+Number = int(input("Type an integer between 1 and 100"))
+
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+
+print(Number + Lucky_Number)
+Addition = (Number + Lucky_Number)
+
+#6. Take the result from #5 and divide it by 2. Print the result.
+
+print(Addition / 2)
+
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+
+Answer = False
+
+#8. Print the value of the boolean variable.
+
+print(Answer)
+
+#9. Create a variable with a number that contains decimals.
+
+Decimal = 5.3
+
+#10. Round the number from #9 up or down using the round function.
+
+print(round(Decimal))
